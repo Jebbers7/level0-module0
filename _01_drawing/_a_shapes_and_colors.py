a5c4e193614a10b5d35b3890bb43f9b337ce3687
jeremycui2008 <jeremycuid@gmail.com>
2022-09-23 18:31:53 -0700

    module 0 3b

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,32 +5,63 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    Jimmy = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    Jimmy.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    Jimmy.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    Jimmy.color('green')
+    Jimmy.pencolor('blue')
     # Move your turtle forward using .forward(100)
     # TEST    Did your turtle move forward?
-
+    for i in range(4):
+        Jimmy.forward(100)
     # Move your turtle left or right using .left(90) or .right(90)
-
+        Jimmy.left(90)
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
 
     # Move your turtle to a new place on the screen using .goto(x, y)
     # x=0 and y=0 is the center of the screen
-
+    Jimmy.goto(200,200)
     # Have your turtle draw a circle using .circle(radius, steps=30)
     # TEST    Did your turtle draw a circle?
-
+    Jimmy.pencolor('green')
+    Jimmy.begin_fill()
+    Jimmy.circle(25, 30)
+    Jimmy.end_fill()
     # Add color to your shape by adding .begin_fill() before drawing the circle
     # and .end_fill() below
 
     # Draw 3 more shapes with different fill colors!
+    Jimmy.pencolor('red')
+    Jimmy.fillcolor('red')
+    Jimmy.goto(-100, -100)
+    Jimmy.begin_fill()
+    for i in range(8):
+        Jimmy.forward(20)
+        Jimmy.left(45)
+    Jimmy.end_fill()
+
+    Jimmy.pencolor('orange')
+    Jimmy.fillcolor('orange')
+    Jimmy.goto(-200,-200)
+    Jimmy.begin_fill()
+    for i in range(6):
+        Jimmy.forward(40)
+        Jimmy.left(60)
+    Jimmy.end_fill()
+
+    Jimmy.pencolor('magenta')
+    Jimmy.fillcolor('magenta')
+    Jimmy.goto(50, -25)
+    Jimmy.begin_fill()
+    for i in range(3):
+        Jimmy.forward(60)
+        Jimmy.left(120)
+    Jimmy.end_fill()
 
     # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
--- _02_code_flow/_c_flaming_ninja_star.py
+++ _02_code_flow/_c_flaming_ninja_star.py
@@ -21,49 +21,49 @@ if __name__ == '__main__':
     flameSize = 130         # the length of the flaming arms
     
     # Make a new turtle
-    
+    walter = turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    
+    walter.shape('turtle')
     # Set the turtle width to 2
-    
+    walter.width(2)
     # Set the turtle speed to 0 (fastest)
-    
+    walter.speed(0)
     # Use a for loop to repeat all of the code below ONE time (we will change
     # this later)
-        
+    for i in range(25):
         # Set the turtle .fillcolor() to orange
-        
+        walter.fillcolor('orange')
         # Call the turtle .begin_fill() function
-        
+        walter.begin_fill()
         # TURN RIGHT     Turn the turtle 1/8 of a circle (hint: 360 degrees
         #                will turn a full circle)
-        
+        walter.right(45)
         # DRAW           Move the turtle 64 pixels
-        
+        walter.forward(64)
         # TURN LEFT      Turn the turtle 40 degrees to the LEFT. (Negative
         #                numbers will turn the turtle counter-clockwise.)
-        
+        walter.left(40)
         # DRAW FLAME     Move the turtle the distance in the variable flameSize
-        
+        walter.forward(flameSize)
         #                Turn the turtle to the right 170 degrees
-         
+        walter.right(170)
         #                Move the turtle the distance in the variable flameSize (again)
-         
+        walter.forward(flameSize)
         #  TURN RIGHT    Turn the turtle 62 degrees to the right
-        
+        walter.right(62)
         #  DRAW          Move the turtle the distance in the variable baseSize
-        
+        walter.forward(baseSize)
         # Call the turtle .end_fill() method
-        
+        walter.end_fill()
     # Hide your turtle so you can see the pattern.
-        
+    walter.hideturtle()
     # TEST   Run the program. Check that your shape is the same as the first
     #        picture in the recipe. This is one arm of the ninja star.
 
     # COLOR  Change the turtle's pen color so that the flame is a different
     #        color to the rest of the star. Run the program again. Check the
     #        second picture in the recipe.
-
+    walter.pencolor('black')
     # LOOP   When you have one arm looking right, change your for loop to
     #        repeat 25 times.
     
